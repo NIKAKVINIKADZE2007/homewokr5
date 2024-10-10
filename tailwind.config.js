5fe8b3c6bb9200f5cfe7947ea765287b42893f95
nikakvinikadze <160760361+NIKAKVINIKADZE2007@users.noreply.github.com>
2024-10-10 19:13:46 +0400

    Revert "Section 3"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    screens: {
-      'laptop-xl': '1440px',
-      'phone-small': '390px',
-    },
-    fontFamily: {
-      manrope: ['manrope', 'sans-serif'],
-    },
     extend: {},
   },
   plugins: [],
